353ecd3221f3d4a59f4720bd77366a6447b927f0
kirillzyusko <zyusko.kirik@gmail.com>
2020-03-17 17:32:59 +0300

    [DL-04] - working app (skip cropping for now)

--- App.tsx
+++ App.tsx
@@ -9,12 +9,12 @@ import {
     TouchableOpacity
 } from 'react-native'
 import * as tf from '@tensorflow/tfjs'
-import { fetch, bundleResourceIO, decodeJpeg } from '@tensorflow/tfjs-react-native'
-import * as mobilenet from '@tensorflow-models/mobilenet'
+import { bundleResourceIO } from '@tensorflow/tfjs-react-native'
 import * as jpeg from 'jpeg-js'
 import RNFetchBlob from 'rn-fetch-blob'
 import ImagePicker from 'react-native-image-picker'
 import ImageResizer from 'react-native-image-resizer';
+import { Tensor } from "@tensorflow/tfjs-core";
 
 import modelJson from './assets/model/model.json'
 import modelWeights from './assets/model/group1-shard1of1.bin'
@@ -24,131 +24,50 @@ type Props = {};
 type State = {
     isTfReady: boolean;
     isModelReady: boolean;
+    predictions: string;
+    image: {
+        uri: string;
+    };
+};
+
+const mapPredictionToLabel = async (tensors: Tensor[]): Promise<string> => {
+    const lengthTensor = await tensors[0].argMax(-1).data();
+    const length = Number(lengthTensor.toString());
+    let str = '';
+
+    for (let i = 1; i < length + 1; i++) {
+        const digit = await tensors[i].argMax(-1).data();
+        str += Number(digit.toString())
+    }
+
+    return str;
 };
 
 class App extends PureComponent<Props, State> {
     private model: tf.LayersModel | null = null;
 
-    state = {
+    public state = {
         isTfReady: false,
         isModelReady: false,
         predictions: null,
         image: null
     };
 
-    async componentDidMount() {
+    public async componentDidMount(): Promise<void> {
         await tf.ready();
-        this.setState({
-            isTfReady: true
-        });
+        this.setState({ isTfReady: true });
+
         try {
-            this.model = await tf.loadLayersModel(bundleResourceIO(modelJson, modelWeights), { strict: false });
+            this.model = await tf.loadLayersModel(bundleResourceIO(modelJson, modelWeights));
         } catch (e) {
-            console.log(1, e)
+            console.error(e);
         }
+
         this.setState({ isModelReady: true });
-
-        //////////////////////////////////
-
-        const image = require('./assets/1.jpg');
-        console.log(1)
-        const imageAssetPath = Image.resolveAssetSource(image);
-        console.log(2, imageAssetPath.uri)
-        const data = await ImageResizer.createResizedImage('https://realt.by/uploads/tx_uedbhouses/Belarus/5102/1301116904/36/eed358d58c54a818.jpg', 64, 64, 'JPEG', 1);
-        console.log(9, data);
-        const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
-        console.log(3, response)
-        const imageData = await response.arrayBuffer();
-        const imageTensor2 = this.imageToTensor(imageData)
-        console.log(5, imageTensor2)
-        console.log(6, imageTensor2.reshape([1, 797, 1200, 3]));
-        console.log(4, imageData)
-
-        // const imageTensor = decodeJpeg(imageData);
-
-        const prediction = await this.model.predict(imageTensor2.reshape([1, 797, 1200, 3]));
-
-// Use prediction in app.
-        this.setState({
-            prediction,
-        });
     }
 
-    imageToTensor(rawImageData) {
-        const TO_UINT8ARRAY = true
-        const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY);
-        // Drop the alpha channel info for mobilenet
-        const buffer = new Uint8Array(width * height * 3);
-        let offset = 0 // offset into original data
-        for (let i = 0; i < buffer.length; i += 3) {
-            buffer[i] = data[offset];
-            buffer[i + 1] = data[offset + 1];
-            buffer[i + 2] = data[offset + 2];
-
-            offset += 4
-        }
-
-        return tf.tensor3d(buffer, [height, width, 3])
-    }
-
-    classifyImage = async () => {
-        try {
-            const imageAssetPath = Image.resolveAssetSource(this.state.image)
-            const response = await fetch(imageAssetPath.uri, {}, { isBinary: true })
-            const rawImageData = await response.arrayBuffer()
-            const imageTensor = this.imageToTensor(rawImageData)
-            const predictions = await this.model.classify(imageTensor)
-            this.setState({ predictions })
-            console.log(predictions)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    selectImage = async () => {
-        ImagePicker.launchCamera({ noData: true, maxHeight: 64, maxWidth: 64 }, async (response) => {
-            console.log(response);
-            try {
-                // add cropping
-                const data = await ImageResizer.createResizedImage(response.path, 64, 64, 'JPEG', 100);
-                console.log(1, data);
-                const path = response.uri;
-                const response2 = await RNFetchBlob.fs.readFile(data.path, 'ascii');
-                //const response = await fetch(`file://${data.path}`, {}, { isBinary: true });
-                const imageData = response2;
-                const imageTensor2 = this.imageToTensor(imageData);
-                console.log(1, imageTensor2)
-                console.log(3, tf.tensor([1, 2, 3, 4]).print());
-                console.log(33, imageTensor2.div(255).print());
-                const prediction = await this.model.predict(imageTensor2.reshape([1, 64, 64, 3]));
-                Object.keys(prediction[0]).forEach((el) => console.log(el));
-                prediction[0].print();
-                prediction[1].print();
-                prediction[2].print();
-                prediction[3].print();
-                prediction[4].print();
-                console.log(4, prediction)
-            } catch (e) {
-                console.log(2, e);
-            }
-            if (!response.cancelled) {
-                const source = { uri: response.uri }
-                this.setState({ image: source })
-                this.classifyImage()
-            }
-        });
-    }
-
-    renderPrediction = prediction => {
-        return (
-            <Text key={prediction.className} style={styles.text}>
-                {prediction.className}
-            </Text>
-        )
-    }
-
-    render() {
-        const { isTfReady, isModelReady, predictions, image } = this.state
+    public render(): JSX.Element {
+        const { isTfReady, isModelReady, predictions, image } = this.state;
 
         return (
             <View style={styles.container}>
@@ -173,22 +92,62 @@ class App extends PureComponent<Props, State> {
                     {image && <Image source={image} style={styles.imageContainer} />}
 
                     {isModelReady && !image && (
-                        <Text style={styles.transparentText}>Tap to choose image</Text>
+                        <Text style={styles.transparentText}>Tap to capture image</Text>
                     )}
                 </TouchableOpacity>
                 <View style={styles.predictionWrapper}>
                     {isModelReady && image && (
                         <Text style={styles.text}>
-                            Predictions: {predictions ? '' : 'Predicting...'}
+                            Predictions: {predictions}
                         </Text>
                     )}
-                    {isModelReady &&
-                    predictions &&
-                    predictions.map(p => this.renderPrediction(p))}
                 </View>
             </View>
         )
     }
+
+    private makePrediction = async (response) => {
+        this.setState({ predictions: 'Processing...' });
+
+        const imageData = await RNFetchBlob.fs.readFile(response.path, 'ascii');
+        const tensor =
+            this.imageToTensor(imageData)
+                .reshape([1, 64, 64, 3])
+                .div(255);
+        const prediction = await this.model.predict(tensor) as Tensor[];
+        const label = await mapPredictionToLabel(prediction);
+
+        this.setState({
+            predictions: label,
+        });
+    };
+
+    private imageToTensor = (rawImageData) => {
+        const TO_UINT8ARRAY = true;
+        const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY);
+        // Drop the alpha channel info for mobilenet
+        const buffer = new Uint8Array(width * height * 3);
+        let offset = 0 // offset into original data
+        for (let i = 0; i < buffer.length; i += 3) {
+            buffer[i] = data[offset];
+            buffer[i + 1] = data[offset + 1];
+            buffer[i + 2] = data[offset + 2];
+
+            offset += 4
+        }
+
+        return tf.tensor3d(buffer, [height, width, 3])
+    };
+
+    private selectImage = async () => {
+        ImagePicker.launchCamera({ noData: true, maxHeight: 64, maxWidth: 64 }, async (response) => {
+            if (!response.didCancel) {
+                const source = { uri: response.uri };
+                this.setState({ image: source });
+                await this.makePrediction(response)
+            }
+        });
+    };
 }
 
 const styles = StyleSheet.create({
@@ -253,6 +212,6 @@ const styles = StyleSheet.create({
         width: 125,
         height: 70
     }
-})
+});
 
 export default App
